1996643cde73475a7b70589dd4282fede4373444
Kozuki Oden <147463361+Lampzzz@users.noreply.github.com>
2024-10-12 17:11:35 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
-"use client";
-
 import { useEffect } from "react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 import useAuthStore from "@/store/authStore";
 import Login from "./(auth)/login/page";
 
 export default function Page() {
   const { isLoading, isAuthenticated, initializeAuthListener } = useAuthStore();
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = initializeAuthListener();
     return () => unsubscribe();
   }, [initializeAuthListener]);
 
-  if (!isLoading && isAuthenticated) redirect("/dashboard");
+  useEffect(() => {
+    if (!isLoading && isAuthenticated) {
+      router.push("/dashboard");
+    }
+  }, [isLoading, isAuthenticated, router]);
+
+  if (isLoading) return <div>Loading...</div>;
 
   return <Login />;
 }
